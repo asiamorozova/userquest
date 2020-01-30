f59963f7f3f62ff2651ef72b8865298882a5c17b
asiamorozova <57855398+asiamorozova@users.noreply.github.com>
2020-01-30 10:59:28 -0800

    changes

---
+++ metadata.js
@@ -0,0 +1,6 @@
+const goblins = {
+    id: 'goblins',
+    title: 'Forest of Goblins',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/b/b3/Goblin_illustration_from_19th_century.jpg'
+    
+}
